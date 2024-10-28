68ac1d67abad5e4724f375d6be892e198a503f7d
fedotova-k <fedotovaekaterina101@gmail.com>
2024-10-28 20:59:16 +0300

    доработка contactDeletion и addContactTest
    добавлены тесты, доработаны предусловия

--- addressbook-web-tests/src/test/java/tests/AddContactTest.java
+++ addressbook-web-tests/src/test/java/tests/AddContactTest.java
@@ -1,42 +1,94 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AddContactTest {
-    private WebDriver driver;
+
+    private static WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        driver = new FirefoxDriver();
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/addressbook/");
+            driver.findElement(By.xpath("//input[@name=\'user\']")).click();
+            driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys("admin");
+            driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
     }
 
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     public void addContactTest() {
-        driver.get("http://localhost/addressbook/addressbook/");
-        driver.manage().window().setSize(new Dimension(1644, 944));
-        driver.findElement(By.xpath("//input[@name=\'user\']")).click();
-        driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys("admin");
-        driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        driver.findElement(By.xpath("//a[contains(text(),\'add new\')]")).click();
+        if (!isElementPresent(By.name("submit"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
         driver.findElement(By.xpath("//input[@name=\'firstname\']")).click();
         driver.findElement(By.xpath("//input[@name=\'firstname\']")).sendKeys("TestFirstName");
         driver.findElement(By.xpath("//input[@name=\'middlename\']")).click();
         driver.findElement(By.xpath("//input[@name=\'middlename\']")).sendKeys("TestMiddleName");
-        driver.findElement(By.xpath("//form[@name=\'theform\']")).click();
         driver.findElement(By.xpath("//input[@name=\'lastname\']")).click();
         driver.findElement(By.xpath("//input[@name=\'lastname\']")).sendKeys("TestLastName");
         driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
         driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
+    }
+
+    @Test
+    public void addContactTestWithEmptyNames() {
+        if (!isElementPresent(By.name("submit"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
+        driver.findElement(By.xpath("//input[@name=\'firstname\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'firstname\']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\'middlename\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'middlename\']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\'lastname\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'lastname\']")).sendKeys("");
+        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
+    }
+
+    @Test
+    public void addContactTestWithAnotherNames() {
+        if (!isElementPresent(By.name("submit"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
+        driver.findElement(By.xpath("//input[@name=\'nickname\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'nickname\']")).sendKeys("test");
+        driver.findElement(By.xpath("//input[@name=\'title\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'title\']")).sendKeys("some company");
+        driver.findElement(By.xpath("//input[@name=\'mobile\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'mobile\']")).sendKeys("875415");
+        driver.findElement(By.xpath("//select[@name=\'bday\']")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bday"));
+            dropdown.findElement(By.xpath("//option[. = '15']")).click();
+        }
+        driver.findElement(By.xpath("//option[@value=\'15\']")).click();
+        driver.findElement(By.xpath("//select[@name=\'bmonth\']")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bmonth"));
+            dropdown.findElement(By.xpath("//option[. = 'September']")).click();
+        }
+        driver.findElement(By.xpath("//input[@name=\'byear\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'byear\']")).sendKeys("2000");
+        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
     }
 }
--- addressbook-web-tests/src/test/java/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/tests/ContactDeletionTest.java
@@ -1,32 +1,58 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ContactDeletionTest {
-  private WebDriver driver;
 
-  @BeforeEach
-  public void setUp() {
-    driver = new FirefoxDriver();
-  }
-  @AfterEach
-  public void tearDown() {
-    driver.quit();
-  }
-  @org.junit.jupiter.api.Test
-  public void contactDeletion() {
-    driver.get("http://localhost/addressbook/addressbook/");
-    driver.manage().window().setSize(new Dimension(1644, 945));
-    driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys("admin");
-    driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-    driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.xpath("//input[@value=\'Delete\']")).click();
-    driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
-  }
+    private static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+
+            driver.get("http://localhost/addressbook/addressbook/");
+            driver.findElement(By.xpath("//input[@name=\'user\']")).click();
+            driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys("admin");
+            driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    @Test
+    public void contactDeletion() {
+        if (!isElementPresent(By.name("home"))) {
+            driver.findElement(By.linkText("home")).click();
+        }
+
+        if (!isElementPresent(By.name("selected[]"))) {
+            driver.findElement(By.linkText("add new")).click();
+            driver.findElement(By.xpath("//input[@name=\'firstname\']")).click();
+            driver.findElement(By.xpath("//input[@name=\'firstname\']")).sendKeys("TestFirstName");
+            driver.findElement(By.xpath("//input[@name=\'middlename\']")).click();
+            driver.findElement(By.xpath("//input[@name=\'middlename\']")).sendKeys("TestMiddleName");
+            driver.findElement(By.xpath("//input[@name=\'lastname\']")).click();
+            driver.findElement(By.xpath("//input[@name=\'lastname\']")).sendKeys("TestLastName");
+            driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+            driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
+        }
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.xpath("//input[@value=\'Delete\']")).click();
+        driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
 }
